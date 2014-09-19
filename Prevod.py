437f6b370671ff30e678628daaf8afade92e4924
gen-maksim <gen-maksim@mail.ru>
2014-09-19 21:40:31 +0400

    Версия 2.0

--- Prevod.py
+++ Prevod.py
@@ -1,18 +1,25 @@
-print ("число 1: ")
-a= int(input())
-print ("система 1: ")
-b= int(input())
-print ("система 2: ")
-d= int(input())
-perehod=0
-c=str(a)
-dlina=len(c)-1
-for i in  c:
-    perehod+= int(i)* (b**dlina)
-    dlina-=1
+while True:
+    print ("_______Начало______")
+    print ("число 1: ")
+    a= int(input())
+    print ("система 1: ")
+    b= int(input())
+    print ("система 2: ")
+    d= int(input())
+    perehod=0
+    c=str(a)
+    dlina=len(c)-1
+    for i in  c:
+        perehod+= int(i)* (b**dlina)
+        dlina-=1
 
-c=''
-while perehod>0:
-    c=str(perehod%d)+c
-    perehod=perehod//d
-print ("Число ", a ," в ", d ,"-й системе счисления выглядит как:"c)
+    c=''
+    while perehod>0:
+        c=str(perehod%d)+c
+        perehod=perehod//d
+    print ("Число ", a ," в ", d ,"-й системе счисления выглядит как:",c)
+    print ("""Чтобы закончить программу введите "N". \r\nЧтобы продолжить нажмите Enter.""")
+    vihod = input()
+    vihod = vihod.upper()
+    if vihod == "N":
+        break
